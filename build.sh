b45ebffbbff24d02bec6444f9274c639fa9d7b61
alexlikescpp <2jes1366@gmail.com>
2025-05-21 14:50:48 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-clang++ cp.cpp -o cp
-clang++ cat.cpp -o cat
-clang++ clear.cpp -o clear
-clang++ echo.cpp -o echo
-clang++ uname.cpp -o uname
-clang++ mv.cpp -o mv
-clang++ ls.cpp -o ls
+for file in *.cpp; do
+  g++ $file -o $(basename "$file" .cpp)
+done
 echo "build complete"
 ls
