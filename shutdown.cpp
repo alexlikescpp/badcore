59048a76f363776eba061423a04430848e22adfe
alexlikescpp <2jes1366@gmail.com>
2025-05-21 15:32:18 -0700

    Update shutdown.cpp

--- shutdown.cpp
+++ shutdown.cpp
@@ -1,11 +1,1 @@
-#include <unistd.h>
-#include <sys/reboot.h>
-#include <cstring>
-int main(int c,char**v){
-    int cmd=RB_POWER_OFF;
-    if(c>1)
-        if(!strcmp(v[1],"halt"))cmd=RB_HALT_SYSTEM;
-        else if(!strcmp(v[1],"reboot"))cmd=RB_AUTOBOOT;
-    sync();
-    reboot(cmd);
-}
+
