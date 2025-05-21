ab22574ef3013ccebf8ac7afc3f002a3ca9eac05
alexlikescpp <2jes1366@gmail.com>
2025-05-21 15:08:52 -0700

    Update chmod.cpp

--- chmod.cpp
+++ chmod.cpp
@@ -1,8 +1,10 @@
 #include <sys/stat.h>
 #include <string>
 
+using namespace std;
+
 int main(int c, char** v) {
-    if (c < 3 || std::string(v[1]) != "+x") return 1;
+    if (c < 3 || string(v[1]) != "+x") return 1;
     struct stat st;
     if (stat(v[2], &st)) return 1;
     return chmod(v[2], st.st_mode | S_IXUSR);
